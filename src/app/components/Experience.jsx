cfe5045cb472c200575e3fc9076f3688a9f3fc6e
nixoletas <nicholetas@gmail.com>
2023-12-16 07:17:25 -0400

    atualizacao de links e traducao de about

--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -10,51 +10,50 @@ const Experience = () => {
 
                 <div id="experience_1" className='my-12'>
                 <h1 className='text-2xl mb-2 text-purple-300'>
-                    Comandante do Pelotão de Sistemas
+                    Systems Platoon Commander
                 </h1>
                 <span className='flex font-sourcecode my-2'>jun 2023 - moment</span>
-                <p className='text-justify text-lg'>Como Comandante do Pelotão de Sistemas, atuei juntamente com a equipe no gerenciamento e manutenção dos sistemas de Comando e Controle do Exército Brasileiro. Também desenvolvi aplicações web em React.js para aprimorar e facilitar a navegação entre esses sistemas.</p>
+                <p className='text-justify text-lg'>As the Systems Platoon Commander, I worked together with the team in managing and maintaining the Brazilian Army's Command and Control systems. I also developed web applications in React.js to improve and facilitate navigation between various different systems.</p>
                 </div>
 
                 <div className='w-1 h-24 mx-12 bg-purple-400 flex justify-center rounded-full'></div>
 
                 <div id="experience_2" className='my-12'>
                 <h1 className='text-2xl mb-2 text-purple-300'>
-                    Chefe da Seção de Tecnologia da Informação
+                Head of Information Technology
                 </h1>
                 <span className='flex font-sourcecode my-2'>jan 2023 - may 2023</span>
-                <p className='text-justify text-lg'>Ainda no 9º B Com GE, chefiei a Seção de Tecnologia da Informação. Com a equipe, pude ver na prática o funcionamento da metodologia Scrum. Atuamos em diversas ferramentas de System Management e Cybersecurity como Proxmox, Graylog, PfSense, Security Onion, entre outros.Ainda no 9º B Com GE, chefiei a Seção de Tecnologia da Informação. Com a equipe, pude ver na prática o funcionamento da metodologia Scrum. Atuamos em diversas ferramentas de System Management e Cybersecurity como Proxmox, Graylog, PfSense, Security Onion, entre outros.
-Competências: DNS (Sistema de nomes de domínio) · pfSense · Scrum</p>
+                <p className='text-justify text-lg'>Still in 9th Batallion, I headed the Information Technology Section. With the team, I was able to see how the Scrum methodology works in practice. We worked on several System Management and Cybersecurity tools such as Proxmox, Graylog, PfSense, Security Onion, among others. We also worked with several SysAdmin and Cybersecurity tools such as Proxmox, Graylog, PfSense, Security Onion, among others.</p>
                 </div>
 
                 <div className='w-1 h-24 mx-12 bg-purple-400 flex justify-center rounded-full'></div>
 
                 <div id="experience_3" className='my-12'>
                 <h1 className='text-2xl mb-2 text-purple-300'>
-                    Chefe da Carteira de Justiça e Disciplina
+                    Head of Justice and Law Section
                 </h1>
                 <span className='flex font-sourcecode my-2'>july 2020 - dec 2022</span>
-                <p className='text-justify text-lg'>Como Comandante do Pelotão de Sistemas, atuei juntamente com a equipe no gerenciamento e manutenção dos sistemas de Comando e Controle do Exército Brasileiro. Também desenvolvi aplicações web em React.js para aprimorar e facilitar a navegação entre esses sistemas.</p>
+                <p className='text-justify text-lg'>As head of the Justice and Discipline Section of the 9th Communications and Electronic Warfare Battalion, I was responsible for administering, managing and resolving all administrative processes related to the Battalion. Managed the investigation scale, administrative processes, documents verifying the validity and veracity of documents, and processes mapping.</p>
                 </div>
 
                 <div className='w-1 h-24 mx-12 bg-purple-400 flex justify-center rounded-full'></div>
 
                 <div id="experience_4" className='my-12'>
                 <h1 className='text-2xl mb-2 text-purple-300'>
-                    Chefe da Seção de Comunicações
+                Head of Communications Section
                 </h1>
                 <span className='flex font-sourcecode my-2'>july 2019 - jun 2020</span>
-                <p className='text-justify text-lg'>Na Base de Administração e Apoio do Comando Militar do Oeste, estive à frente da Seção de Comunicações, responsável pela disponibilidade e operacionalidade de todos os rádios Motorola APX 2000 do serviço interno ao Forte Pantanal. Também atuei na formação de reservistas, ministrando instruções dos mais variados temas relacionados à atividade da caserna para os soldados incorporados.</p>
+                <p className='text-justify text-lg'>At the Administration and Support Base of the Western Military Command, I was in charge of the Communications Section, responsible for the availability and operability of all Motorola APX 2000 radios for the internal services at Fort Pantanal. I also worked on training reservists, giving instructions on a wide range of topics related to army activities for soldiers.</p>
                 </div>
 
                 <div className='w-1 h-24 mx-12 bg-purple-400 flex justify-center rounded-full'></div>
 
                 <div id="experience_5" className='my-12'>
                 <h1 className='text-2xl mb-2 text-purple-300'>
-                    Armed Forces Officer
+                    Commander of Communications Platoon
                 </h1>
                 <span className='flex font-sourcecode my-2'>july 2018 - jun 2019</span>
-                <p className='text-justify text-lg'>Como Comandante do Pelotão de Comunicações do 20º Regimento de Cavalaria Blindado, Campo Grande, MS, estive desenvolvendo atributos de liderança e trabalho em equipe para cumprir as missões diárias e de grande vulto ocorridas no âmbito do regimento. No pelotão, trabalhávamos provendo o enlace e manutenção de infovias espalhadas por todo o centro-oeste, também possuíamos equipamentos rádio Motorola e Harris e realizávamos a manutenção e configuração desses equipamentos.</p>
+                <p className='text-justify text-lg'>As Commander of the Communications Platoon of the 20th Armored Cavalry Regiment, Campo Grande, MS, I was developing leadership and teamwork attributes to carry out the daily and major missions carried out within the regiment. In the platoon, we worked providing the link and maintenance of infoways spread throughout the Midwest, we also had Motorola and Harris radio equipment and carried out the maintenance and configuration of this equipment.</p>
                 </div>
 
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,7 +25,7 @@ const page = () => {
         <h1 className='wrapper textAnimate glow-effect border-[2px] max-w-xl rounded-lg p-3 border-violet-600'>Hello, my name is <span className='text-violet-400 font-semibold'>Nicholas! --</span> i&apos;m a Software Developer based in Brazil!</h1>
         <div className='flex flex-row justify-center text-left pt-8'>
           <div>
-            <h1 className='text-4xl text-white/90 max-md:text-2xl'>Nicholas Miyasato</h1>
+            <h1 className='text-4xl text-white/90 max-md:text-xl'>Nicholas Miyasato</h1>
             <p className='text-white/90 max-md:text-sm'>Digital Craftsman ( Artist / Developer / Designer )</p>
             <TypingAnimation/>
         </div>
@@ -35,28 +35,40 @@ const page = () => {
         </div>
         </div>
         <div className='socials flex justify-center pt-12 gap-4'>
-          <Link href="http://linkedin.com/nixoletas">
+          <Link href="https://www.linkedin.com/in/nixoletas/?locale=en_US" target='_blank'>
             <Image className="shiny-border rounded-xl p-1 hover:scale-105 hover:-translate-y-1" src="/linkedin-svgrepo-com.png"
             width={40}
             height={40} alt="linkedin">
             </Image>
           </Link>
-          <Link href="https://github.com/nixoletas">
+          <Link href="https://github.com/nixoletas" target='_blank'>
             <Image className="shiny-border rounded-xl p-1 hover:scale-105 hover:-translate-y-1" src="/github-purple.png"
             width={40}
             height={40} alt="github-icon">
             </Image>
           </Link>
-          <Link href="http://linkedin.com/nixoletas">
+          <Link href="mailto:nick.miyasato.freela@gmail.com">
             <Image className="shiny-border rounded-xl p-1 hover:scale-105 hover:-translate-y-1" src="/email-svg.png"
             width={40}
             height={40} alt="email-icon">
             </Image>
           </Link>
-          <Link href="http://instagram.com/nixoletas">
+          <Link href="http://instagram.com/nixoletas" target='_blank'>
             <Image className="shiny-border rounded-xl p-1 hover:scale-105 hover:-translate-y-1" src="/instagram-svg.png"
             width={40}
             height={40} alt="insta-icon">
+            </Image>
+          </Link>
+          <Link href="http://youtube.com/nixoletas" target='_blank'>
+            <Image className="shiny-border rounded-xl p-1 hover:scale-105 hover:-translate-y-1" src="/youtube-svg.png"
+            width={40}
+            height={40} alt="youtube-icon">
+            </Image>
+          </Link>
+          <Link href="http://soundcloud.com/nixoletas" target='_blank'>
+            <Image className="shiny-border rounded-xl p-1 hover:scale-105 hover:-translate-y-1" src="/soundcloud-svg.png"
+            width={40}
+            height={40} alt="soundcloud-icon">
             </Image>
           </Link>
         </div>
