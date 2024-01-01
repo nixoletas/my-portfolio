b59d91869b3e4bdfb3e0794aa81465e8b42bfc6a
nixoletas <nicholetas@gmail.com>
2024-01-01 10:22:07 -0400

    added google adsense

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,9 @@
 import { Oswald } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const inter = Oswald({ subsets: ['latin'], weight:['400'] })
 
@@ -12,6 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body className={inter.className}>{children}
       
       <Analytics/>
