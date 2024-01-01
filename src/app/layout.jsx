3a0f5ac08deda5db418f72af2f441f09cc524edf
nixoletas <nicholetas@gmail.com>
2024-01-01 08:46:26 -0400

    added vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Oswald } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Oswald({ subsets: ['latin'], weight:['400'] })
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      
+      <Analytics/>
+      </body>
     </html>
   )
 }
